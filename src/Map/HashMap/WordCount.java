c1273383d54eb9158e7155f7492dbee437d11ba7
gulshensettarova <gulshensettarova@gmail.com>
2024-09-14 17:47:48 +0400

    Map documentation and examples

---
+++ src/Map/HashMap/WordCount.java
@@ -0,0 +1,31 @@
+package Map.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordCount {
+    public static void main(String[] args) {
+        // Söz sayımı üçün Map yaradın
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        // Mətnin içindəki sözlər
+        String text = "hello world hello java world java";
+        String[] words = text.split(" ");
+
+        // Sözlərin sayını artırın
+        //wordCount.getOrDefault(word, 0) metodu istifadə edərək,
+        // əgər wordCount map-də həmin sözü taparsa, onun cari sayını alır, əks halda 0 qaytarır.
+        //wordCount.put(word, wordCount.getOrDefault(word, 0) + 1) ilə həmin sözün sayını 1 artıraraq
+        // Map-ə yerləşdirir. Əgər həmin söz əvvəllər map-də varsa, onun sayı artırılır;
+        // yoxdursa, yeni açar-dəyər cütü əlavə edilir.
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        // Sözlərin sayını göstərin
+        System.out.println("Word count:");
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+}
